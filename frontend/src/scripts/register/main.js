16657cf465f1d6fac9b18ed299ef320088b95d64
StarLordCraft <rafa.viana.moc@gmail.com>
2023-06-20 21:18:10 -0300

    validando campos

--- frontend/src/scripts/register/main.js
+++ frontend/src/scripts/register/main.js
@@ -1,3 +1,23 @@
-import verifyEmail from './verifyEmail';
+function verifyCamp(evt){
+    const camp = evt.target;
+    const id = camp.getAttribute('id');
+    
+    if(camp.validity.tooShort){
+        const messageError = document.querySelector(`#${id} ~ p`);
+        messageError.classList.add('errorMessage')
+        messageError.innerHTML = 
+        `${id.toUpperCase()} deve conter no mínimo 7 caracteres`;
+        
+        camp.classList.add('error');
+    }
+}
 
+const usernameCamp = document.getElementById('username');
 const emailCamp = document.getElementById('email');
+const passwordCamp = document.getElementById('password');
+
+usernameCamp.addEventListener('focusout', verifyCamp);
+usernameCamp.addEventListener('focusin', () => {
+    usernameCamp.classList.remove('error');
+})
+
