5b1a31b39f780ae756f0bd8101e1b32f5c8ceccc
StarLordCraft <rafa.viana.moc@gmail.com>
2023-06-21 14:34:01 -0300

    Frontend

--- backend/request.js
+++ backend/request.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const app = express();
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
+const express = require('express');
 
 //register
+function httpRequests(app){
+    app.use(express.json());
+    
+    app.post('/register/auth', async(req, res) => {
+        const { username, email, password } = req.body;
+        res.send(username, email, password)
+    })
+}
 
-app.use(express.json());
-
-app.post('/register/auth', async(req, res) => {
-    const { username, email, password } = req.body;
-    res.send(username, email, password)
-})
-
+module.exports = {httpRequests};
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 require('dotenv').config();
+
 const express = require('express');
 const app = express();
 const path = require('path');
 const rotas = require('./rotas');
+
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt');
+const { httpRequests } = require('./request');
 
 const mainPath = __dirname.split('\\backend')[0];
 
@@ -18,10 +21,6 @@ const dbUser = process.env.DB_USER;
 const dbPass = process.env.DB_PASS;
 const dataBaseUrl = `mongodb+srv://${dbUser}:${dbPass}@cluster0.st1r4mu.mongodb.net/?retryWrites=true&w=majority`;
 
-app.post('/auth/register', (req, res) => {
-    let {username, email, password} = req.body;
-    console.log(req.body)
-    res.send({username, email, password})
-})
-
 app.listen(3000, () => {console.log('server rodando')})
+
+httpRequests(app);
