7bc457b8aa761851fcb90d8ada4b670b620ba3c9
Eder Fonseca <eder@devmagic.com.br>
2023-06-21 00:16:30 -0300

    Banco dados

---
+++ backend/request.js
@@ -0,0 +1,10 @@
+const app = express();
+const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+//register
+app.post('/auth/register', async(req, res) => {
+    const { username, email, password } = req.body;
+
+})
+
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,27 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const path = require('path');
 const rotas = require('./rotas');
+const mongoose = require('mongoose')
+const bcrypt = require('bcrypt');
 
 const mainPath = __dirname.split('\\backend')[0];
 
 app.use(express.static(path.join(mainPath + '/frontend/src')));
+app.use(express.json());
 
 app.use(rotas);
 app.use((req, res)=>{
-    res.status(404).send('Página não encontrada seu merda!')
+    res.status(404).send('filha da puta')
 })
 
-app.listen(3000, () => {console.log('server running')});
+//conexão banco de dados
+const dbUser = process.env.DB_USER;
+const dbPass = process.env.DB_PASS;
+const dataBaseUrl = `mongodb+srv://${dbUser}:${dbPass}@cluster0.st1r4mu.mongodb.net/?retryWrites=true&w=majority`;
+
+mongoose.connect(dataBaseUrl)
+    .then(
+        app.listen(3000, () => { console.log('Banco conectado!') })
+    ).catch((err) => { console.log(err) })
