07bb8771fef46255a5b0b4f038564a53701aec8a
StarLordCraft <rafa.viana.moc@gmail.com>
2023-06-21 04:08:07 -0300

    bux Fixing

--- backend/request.js
+++ backend/request.js
@@ -1,9 +1,13 @@
+const express = require('express')
 const app = express();
 const bcrypt = require('bcrypt')
 const jwt = require('jsonwebtoken')
 
 //register
-app.post('/auth/register', async(req, res) => {
+
+app.use(express.json());
+
+app.post('/register/auth', async(req, res) => {
     const { username, email, password } = req.body;
     res.send(username, email, password)
 })
--- frontend/src/scripts/register/main.js
+++ frontend/src/scripts/register/main.js
@@ -1,7 +1,7 @@
 function verifyCamp(evt){
     const camp = evt.target;
     const id = camp.getAttribute('id');
-    
+
     if(camp.validity.tooShort){
         const messageError = document.querySelector(`#${id} ~ p`);
         messageError.classList.add('errorMessage')
@@ -16,6 +16,18 @@ const usernameCamp = document.getElementById('username');
 const emailCamp = document.getElementById('email');
 const passwordCamp = document.getElementById('password');
 
+const inputs = [...document.querySelectorAll('input')];
+
+inputs.map((e) => {
+    e.addEventListener('input', function (){
+        if(this.value !== ""){
+            this.nextElementSibling.style.transform = "translateY(-10px)";
+        }else {
+            this.nextElementSibling.style.transform = "translateY(0)";
+        }
+    })
+})
+
 usernameCamp.addEventListener('focusout', verifyCamp);
 usernameCamp.addEventListener('focusin', () => {
     usernameCamp.classList.remove('error');
@@ -23,20 +35,25 @@ usernameCamp.addEventListener('focusin', () => {
 
 
 
-document.querySelector('.button-register', () => {
+document.querySelector('.button-register').addEventListener('click', (e) =>{
+    e.preventDefault();
     const dados = {
         username: usernameCamp.value,
         email: emailCamp.value,
         password: passwordCamp.value,
-    
     }
+
+    console.log(dados)
 
     const cabecalho = {
         method: 'POST',
-        header:{
-            'content-type': 'aplication-json',
+        headers: {
+            'Content-Type': 'application/json',
         },
         body: JSON.stringify(dados),
     }
-    fetch('/auth/register', cabecalho)    
+
+    fetch('/auth/register', cabecalho)
+    .then(res => res.json())
+    .then(res => console.log(res)); 
 })
